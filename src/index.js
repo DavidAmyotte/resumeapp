3fc767b314d7d92909836fddda35d1870edb8658
DavidAmyotte <damyotte21@gmail.com>
2021-02-13 18:31:55 -0600

    Commiting project changesd

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+import { PDFViewer } from 'react-view-pdf';
+
+
+class Viewer extends Component {
+    render() {
+        return ( <
+            PDFViewer url = "/damyotte.pdf" / >
+        );
+    }
+}
+
+
+ReactDOM.render( < Viewer / > ,
+    document.getElementById('root')
+);
